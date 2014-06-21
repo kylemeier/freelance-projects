f58a9c6fdde17919a62c7f9017a9e9ff108a86f9
Kyle Meier <kyledmeier@gmail.com>
2014-06-21 11:51:58 -0700

    general flow of desktop and mobile site sketched out

---
+++ meredithadelaide/js/script.js
@@ -0,0 +1,16 @@
+(function(){
+
+	// $(document).on('click', '#modeling', function(){
+	// 	$('#modeling-list').slideToggle('fast');
+	// });
+	// var $menu = $('#menu'),
+	// 	$sections = $menu.find('li:has(ul)');
+
+	// 	$sections.each(function(){
+	// 		var $trigger = $(this).children('a'),
+	// 			$expandList = $trigger.siblings();
+	// 		$trigger.click(function(){
+	// 			$expandList.slideToggle();
+	// 		});
+	// 	});    
+})();
