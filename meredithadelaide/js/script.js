c19b4865f9b5025b2fbb97dc455ddbe51ae544a7
Kyle Meier <kyledmeier@gmail.com>
2014-06-22 19:29:18 -0700

    removed custom scrollbar in favor of styling scrollbar in webkit, will style IE scrollbar and firefox will have default. adjusted heading in mobile view

--- meredithadelaide/js/script.js
+++ meredithadelaide/js/script.js
@@ -6,11 +6,11 @@
 // 	$(document).scrollLeft(0);
 // 	$('.image-pane').css('left',scrolling + 'px');
 // 	});
-	if($(window).width() > 800){
+// 	
+	$(function(){
 	function init(){
 		if($(window).width() > 800){
 			setWidth();
-			scrollBar();
 		}
 	}
 	function setWidth(){
@@ -21,76 +21,15 @@
 		$('#images').width(imagesWidth);
 	}
 
-	
-
-	function scrollBar(){
-
-		var scrollBarOffset = 20;
- 
-    	var viewportWidth = $('#image-pane').width();
-    	var docuWidth = $('#images').width();
- 
-    	var trackWidth = $('#scrollbar-track').width();
- 
-    	var scrollbarWidth = (viewportWidth/docuWidth)*trackWidth;
-    	
- 		
-    	$('#scrollbar').width(scrollbarWidth);
-		var scrollbarLeft = parseInt($('#scrollbar').css('left'));
-
-				if(scrollbarWidth + scrollbarLeft >= viewportWidth){
-					$('#scrollbar').css('left', viewportWidth - scrollbarWidth);
-					scrollbarLeft = parseInt($('#scrollbar').css('left'));
-				}
-		var scrollLeftNew = (scrollbarLeft/(trackWidth))*docuWidth;
-		
-		$('#image-pane').css('left',-scrollLeftNew);
-		
-		console.log(viewportWidth,docuWidth, scrollbarWidth, scrollbarLeft);
-		console.log(scrollbarWidth + scrollbarLeft);
-		
-
-		$('#scrollbar').draggable({
-			axis: 'x',
-			containment: 'parent',
-			drag: function() {
-				var scrollbarLeft = parseInt($(this).css('left'));
-				var scrollLeftNew = (scrollbarLeft/(trackWidth))*docuWidth;
-				// $('.image-pane').scrollLeft();
-				$('#image-pane').css('left',-scrollLeftNew);
-				// $('.image-pane').css('left',-scrollbarLeft);
-				// $('.images').scrollLeft(100);
-			}
-		});
-		$('body').bind('mousewheel',function(event,delta){
-		var scrollLeft = $('#image-pane').width();
-		var scrollLeftNew = scrollLeft - (delta * 40);
-
-		$('#image-pane').css('left',-scrollLeftNew);
-
-		var scrollbarLeft = ($('#image-pane').scrollLeft()/docuWidth)*trackWidth;
-		$('#scrollbar').css({
-			left: scrollLeft
-		});
-	});
-
-	}
-	$("#image-container").bind("resizestop", function () {
-  		$("#scrollbar").draggable("option", "containment", $("#image-container"));
-	});
-
-
 	init();
-		$(window).resize(function(){
-		if($(window).width() > 800){
-		init();
 
+	$(window).resize(function(){
+		if($(window).width() > 800){
+			init();
 		}else{
-			console.log('here!');
 			$('#images').css('width','100%');
-			$('#image-pane').css('left',0);
 		}
 	});
-	}
-
+});
+	
 })();
