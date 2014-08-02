459c9f2261a45b2faade9737e2777e9c90bcfa28
Kyle Meier <kyledmeier@gmail.com>
2014-08-01 22:04:30 -0700

    finished up files, added favicon

--- meredithadelaide/self/solestruck-yes/index.php
+++ meredithadelaide/self/solestruck-yes/index.php
@@ -5,6 +5,7 @@
 	<title>meredithadelaide - solestruck - yes</title>
 	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
 	<meta http-equiv='X-UA-Compatible' content='IE=9'>
+	<link rel="icon" href="../../favicon.ico" type="image/x-icon" />
 	<link rel='stylesheet' href='../../css/reset.css'>
 	<link rel='stylesheet' href='../../css/style.css'>
 </head>
--- meredithadelaide/video/index.php
+++ meredithadelaide/video/index.php
@@ -2,8 +2,10 @@
 <html lang='en'>
 <head>
 	<meta charset='UTF-8'>
-	<title>video</title>
+	<title>meredith adelaide - video</title>
 	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
+	<meta http-equiv='X-UA-Compatible' content='IE=9'>
+	<link rel="icon" href="../favicon.ico" type="image/x-icon" />
 	<link rel='stylesheet' href='../css/reset.css'>
 	<link rel='stylesheet' href='../css/style.css'>
 </head>
